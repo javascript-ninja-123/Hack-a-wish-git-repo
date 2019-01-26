230eba977b525b5ed88c1bb2c1f49bb984c60dfe
Sung Yi <Sung.Yi@fox.com>
2019-01-25 21:27:24 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './style/index.less';
+import './registerServiceWorker';
+import App from './app';
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
