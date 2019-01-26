98973ed77eacb37c609b4f5c7fe10fd5b4e98294
Lawrence Tucker <lawrence.ed.tucker@gmail.com>
2019-01-26 13:00:30 -0800

    Installed packages and setup folders

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-
 // -- Components
-import Search from 'containers/search';
+import Main from 'containers/main';
 
 
 
-class App extends Component{
-    render(){
-        return(
-          <div>
-            <Search /> {/*TODO: Add Router*/}
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* <Header/> */}
+                <Main /> {/*TODO: Add Router*/}
             </div>
         )
     }
