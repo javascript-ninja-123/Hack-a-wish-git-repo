8b057c97573a410dfd99b4cacfebf48c2cdead94
ajv21990 <AJV21990@gmail.com>
2019-01-26 12:05:19 -0800

    Finished Header

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './containers/header/header'
 
 
 // -- Components
@@ -10,7 +11,8 @@ class App extends Component{
     render(){
         return(
           <div>
-            <Search /> {/*TODO: Add Router*/}
+            <Header />
+            {/* <Search /> TODO: Add Router */}
             </div>
         )
     }
