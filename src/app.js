743680e9d07c1d57760be6e6dae7aa6624a17379
ajv21990 <AJV21990@gmail.com>
2019-01-26 14:10:03 -0800

    merged

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './containers/header/header'
+import { Link } from 'react-router-dom';
 
 
 // -- Components
@@ -11,8 +12,11 @@ class App extends Component{
     render(){
         return(
           <div>
+            <h1>Hack-A-Wish Main Page</h1>
             <Header />
-            {/* <Search /> TODO: Add Router */}
+        <ul>
+          <li><Link to="/search">Search</Link></li>
+        </ul>
             </div>
         )
     }
